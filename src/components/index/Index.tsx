fbc5c0b1ab959bd20a36ed1341c386fcdf115733
nish1013 <nishand2d@gmail.com>
2023-07-29 20:49:07 +0100

    Added links

--- src/components/index/Index.tsx
+++ src/components/index/Index.tsx
@@ -1,8 +1,12 @@
 import React from "react"
+import IndexLink from "./IndexLink"
+import { mainLinks } from "../../data/MainLinks"
 export default function Index() {
   return (
-    <div>
-      hello
+    <div className="flex flex-col items-center">
+        {
+            mainLinks.map((l,i) => <IndexLink key={i} url={l.url} text={l.text} />)
+        }
     </div>
   )
 }
