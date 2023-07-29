3f6e61dd0865d86844b67993f7c4e6c9b0e6cd7e
nish1013 <nishand2d@gmail.com>
2023-07-29 14:31:44 +0100

    initial commit

---
+++ gatsby-config.ts
@@ -0,0 +1,23 @@
+import type { GatsbyConfig } from "gatsby";
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Nishan Satharasinghe`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": process.env.CMS_URL
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss"]
+};
+
+export default config;
