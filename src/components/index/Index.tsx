d09a9a201a0b823a2d1ebe49eb13bb2d60453d26
nish1013 <nishand2d@gmail.com>
2023-07-29 21:46:57 +0100

    Added summary

--- src/components/index/Index.tsx
+++ src/components/index/Index.tsx
@@ -2,10 +2,12 @@ import React from "react"
 import IndexLink from "./IndexLink"
 import { mainLinks } from "../../data/MainLinks"
 import ProfileImage from "./ProfileImage"
+import Summary from "./Summary"
 export default function Index() {
   return (
     <div className="flex flex-col items-center">
         <ProfileImage/>
+        <Summary/>
         {
             mainLinks.map((l,i) => <IndexLink key={i} url={l.url} text={l.text} />)
         }
