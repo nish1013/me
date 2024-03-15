8a0b8c7e4a48aa2847028d7b25ad90a12319fc63
nish1013 <nishand2d@gmail.com>
2024-03-15 18:43:29 +0000

    Added CS podcast

--- src/components/podcasts/podcasts.tsx
+++ src/components/podcasts/podcasts.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import "./styles.css"
-import Spotify from './spotify'
+import Spotify from './spotify/spotify'
 import Apple from './apple'
 
 export default function Podcasts() {
