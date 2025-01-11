19b1bde2e3caf5a6c59d162704febeec24753318
nish1013 <nishand2d@gmail.com>
2025-01-11 14:09:48 +0000

    Add additional certifications to the data source

--- src/components/certifications/data.ts
+++ src/components/certifications/data.ts
@@ -12,4 +12,16 @@ export const CERTIFICATIONS: CertificationModel[] = [
     title: 'Microsoft',
     uri: 'https://learn.microsoft.com/en-gb/users/nish1013/achievements',
   },
+  {
+    title: 'LeetCode',
+    uri: 'https://leetcode.com/nish1013',
+  },
+  {
+    title: 'IBM',
+    uri: 'https://www.credly.com/users/nishan-lakmal-satharasinghe',
+  },
+  {
+    title: 'Credly',
+    uri: 'https://www.credly.com/users/nish1013',
+  },
 ];
