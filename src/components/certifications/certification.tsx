043daf29287d9368f70a6e82c07e4529e854b8a2
nish1013 <nishand2d@gmail.com>
2025-01-11 13:56:37 +0000

    Update Certification component styling for improved responsiveness

--- src/components/certifications/certification.tsx
+++ src/components/certifications/certification.tsx
@@ -9,7 +9,7 @@ export default function Certification({ title, uri }: CertificationProps) {
   return (
     <div
       onClick={() => navigate(uri)}
-      className="flex items-center justify-center h-12 w-1/4 mx-auto my-2 cursor-pointer bg-amber-500 hover:bg-amber-400 text-sm text-black font-medium"
+      className="flex items-center justify-center h-12 w-11/12 max-w-lg md:w-1/3 mx-auto my-2 cursor-pointer bg-amber-500 hover:bg-amber-400 text-sm text-black font-medium"
     >
       <p>{title}</p>
     </div>
