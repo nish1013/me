5c01a23df834e6dc61b871f269c8eab407f56acb
nish1013 <nishand2d@gmail.com>
2026-07-03 07:08:52 +0100

    Update alert banner with dark theme

--- src/components/alert/AlertInfo.tsx
+++ src/components/alert/AlertInfo.tsx
@@ -9,29 +9,28 @@ interface AlertInfoProps {
 export default function AlertInfo({ label, title, onClick }: AlertInfoProps) {
   return (
     <div
-      className="p-2 bg-amber-200 items-center text-black-100 leading-none flex lg:inline-flex w-full"
+      className="py-2 px-4 bg-slate-800 items-center text-slate-200 leading-none flex lg:inline-flex w-full"
       role="alert"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke="currentColor"
-        class="w-6 h-6"
+        className="w-5 h-5 text-slate-400"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
         />
       </svg>
-      <span className="font-semibold mr-2 text-center flex-auto text-sm">
+      <span className="ml-2 text-center flex-auto text-sm text-slate-300">
         {title}
       </span>
       <button onClick={() => onClick()}>
-        {' '}
-        <span className="flex rounded-full bg-amber-500 uppercase px-2 py-1 text-xs font-bold mr-3">
+        <span className="rounded-md bg-slate-700 hover:bg-slate-600 uppercase px-3 py-1 text-xs font-medium text-slate-200 transition-colors">
           {label}
         </span>
       </button>
