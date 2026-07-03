cc5bc6685da6bc14972909ed314e848f91f30294
nish1013 <nishand2d@gmail.com>
2026-07-03 07:08:45 +0100

    Improve summary typography and layout

--- src/components/index/Summary.tsx
+++ src/components/index/Summary.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-// TODO: move text to resources.
 const text =
   'Dynamic and collaborative Software Engineer specializing in TypeScript, JavaScript, Node.js, and React.js. Proven track record in developing innovative products for renowned organizations like IBM, Visa, M&S, Virgin, and Telefonica. Strong expertise in full-stack development, microservices architecture, and modernizing predictive analytics processes. Skilled in UI development using React.js and proficient in JavaScript frameworks and libraries. Committed to delivering high-quality solutions while mentoring junior developers.';
 
 export default function Summary() {
-  return <div className="font-sans text-justify my-5">{text}</div>;
+  return (
+    <p className="text-slate-600 text-center leading-relaxed max-w-lg mx-auto my-6 text-sm">
+      {text}
+    </p>
+  );
 }
