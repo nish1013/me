a6f96cabf6f7863ce1cafaa3a9c2d9e5562ea402
nish1013 <nishand2d@gmail.com>
2026-07-03 07:09:01 +0100

    Update page title to Lead Software Engineer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,4 +13,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>Nish | Lead Software Engineer</title>;
