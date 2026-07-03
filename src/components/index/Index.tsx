b7ece213432b38bc87edebc1ecb8b9e4671b5c0b
nish1013 <nishand2d@gmail.com>
2026-07-03 07:18:49 +0100

    Remove summary from homepage for cleaner layout

--- src/components/index/Index.tsx
+++ src/components/index/Index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import IndexLink from './IndexLink';
 import { mainLinks } from '../../data/MainLinks';
 import ProfileImage from './ProfileImage';
-import Summary from './Summary';
 import Specialties from './Specialties';
 
 export default function Index() {
@@ -10,8 +9,7 @@ export default function Index() {
     <div className="flex flex-col items-center py-12 px-6 max-w-2xl mx-auto">
       <ProfileImage />
       <Specialties />
-      <Summary />
-      <div className="flex flex-col items-center mt-4 w-full">
+      <div className="flex flex-col items-center mt-6 w-full">
         {mainLinks.map((l, i) => (
           <IndexLink key={i} url={l.url} text={l.text} />
         ))}
